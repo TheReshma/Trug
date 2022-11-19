b10e848bd762256ca428bc6d5cc37eef5cc5ee2b
TheReshma <reshmashaik3011@gmail.com>
2022-11-19 08:26:18 +0530

    fix: api keys

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    ALCHEMY_KEY: process.env.ALCHEMY_KEY,
+    DEV_ENV: process.env.DEV_ENV,
+    WEB3_STORAGE_TOKEN: process.env.WEB3_STORAGE_TOKEN,
+  },
 };
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,14 +21,12 @@ const { chains, provider, webSocketProvider } = configureChains(
     chain.optimism,
     chain.arbitrum,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
-      ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
+      ? [chain.goerli, chain.sepolia]
       : []),
   ],
   [
     alchemyProvider({
-      // This is Alchemy's default API key.
-      // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+      apiKey: process.env.ALCHEMY_KEY as string,
     }),
     publicProvider(),
   ]
