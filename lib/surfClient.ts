2103d7c400add217ce154492805b73f984c31cf8
TheReshma <reshmashaik3011@gmail.com>
2022-12-20 16:51:14 +0530

    back to http

--- lib/surfClient.ts
+++ lib/surfClient.ts
@@ -1,5 +1,5 @@
 import { SurfClient } from "@surfdb/client-sdk";
 
 export const surfClient = new SurfClient({
-  client: 'https://159.223.50.129:3000',
+  client: 'http://159.223.50.129:3000',
 });
