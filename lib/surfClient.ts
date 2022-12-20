1507a728c0fdacc41ef70e192ac2d0930f133e86
TheReshma <reshmashaik3011@gmail.com>
2022-12-20 16:39:39 +0530

    try https

--- lib/surfClient.ts
+++ lib/surfClient.ts
@@ -1,10 +1,5 @@
 import { SurfClient } from "@surfdb/client-sdk";
 
-// const clientURL =
-//   process.env.NODE_ENV == "production"
-//     ? ""
-//     : "http://localhost:4200";
-
 export const surfClient = new SurfClient({
-  client: 'http://159.223.50.129:3000',
+  client: 'https://159.223.50.129:3000',
 });
