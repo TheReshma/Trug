aad3c82251155adfb0917d0c019f5a9de1caf554
TheReshma <reshmashaik3011@gmail.com>
2022-12-20 16:36:17 +0530

    fix: client url

--- lib/surfClient.ts
+++ lib/surfClient.ts
@@ -1,10 +1,10 @@
 import { SurfClient } from "@surfdb/client-sdk";
 
-const clientURL =
-  process.env.NODE_ENV == "production"
-    ? "http://159.223.50.129:3000"
-    : "http://localhost:4200";
+// const clientURL =
+//   process.env.NODE_ENV == "production"
+//     ? ""
+//     : "http://localhost:4200";
 
 export const surfClient = new SurfClient({
-  client: clientURL,
+  client: 'http://159.223.50.129:3000',
 });
